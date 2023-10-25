0a712120c352cb3c50257794e6e4085b418ae0d3
DanilaKisikov <kisikov03@mail.ru>
2023-10-26 01:41:27 +0300

    Finish first 4 parts

--- main.py
+++ main.py
@@ -1,6 +1,66 @@
+import sympy as s
 import utils as u
 
 
+def firstTask():
+    mesh = u.generateCube()
+    u.drawPlot([mesh], "Задание №1")
+
+
+def secondTask():
+    scale = 1.5
+    mesh = u.generateCube()
+    newMesh = u.generateCube(color=u.Color.pink)
+
+    m: s.Matrix = scale * s.eye(4)
+    m[3, 3] = 1
+
+    newMesh.getNewPoints(m)
+    u.drawPlot([mesh, newMesh], "Задание №2")
+
+
+def thirdTask():
+    mesh = u.generateCube()
+    newMesh = u.generateCube(color=u.Color.pink)
+
+    m: s.Matrix = u.getMovingMatrix(-3, 0, 0)
+    newMesh.getNewPoints(m)
+
+    u.drawPlot([mesh, newMesh], "Задание №3")
+
+
+# В трёх плоскостях
+# На вики
+def fourthTask():
+    matrixZ: s.Matrix = u.getRotationMatrix(z=45)
+
+    meshZ = u.generateCube(color=u.Color.red)
+    meshZ.getNewPoints(matrixZ)
+
+    u.drawPlot([meshZ], "Задание №4")
+
+
+def fifthTask():
+    rotationMatrix: s.Matrix = u.getRotationMatrix(y=-45, z=270)
+
+    mesh = u.generateCube()
+    newMesh = u.generateCube(color=u.Color.red)
+
+    pnt = list(newMesh.points[2])
+    print(pnt)
+    movingMatrix: s.Matrix = u.getMovingMatrix(pnt[0], pnt[1], pnt[2])
+    print(movingMatrix)
+
+    finalMatrix: s.Matrix = movingMatrix.inv() * rotationMatrix * movingMatrix
+
+    newMesh.getNewPoints(finalMatrix)
+
+    u.drawPlot([mesh, newMesh], "Задание №4")
+
+
 if __name__ == "__main__":
-    mesh = u.Mesh(u.VERTICES, u.Color.blue, "КУБ ЕБАТЬ!", u.FACES)
-    u.drawPlot([mesh], "ЕБАТЬ")
+    firstTask()
+    secondTask()
+    thirdTask()
+    fourthTask()
+    fifthTask()
--- utils.py
+++ utils.py
@@ -7,7 +7,7 @@ import sympy as s
 from mpl_toolkits.mplot3d import Axes3D
 from mpl_toolkits.mplot3d.art3d import Poly3DCollection
 
-SPACE_LIMIT: Final[float] = 3
+SPACE_LIMIT: Final[float] = 5
 
 FACES = [(1, 0, 2, 3),
          (1, 0, 4, 5),
@@ -18,14 +18,14 @@ FACES = [(1, 0, 2, 3),
          ]
 
 VERTICES = [
-    s.Matrix([0, 0, 0]),
-    s.Matrix([0, 0, 1]),
-    s.Matrix([0, 1, 0]),
-    s.Matrix([0, 1, 1]),
-    s.Matrix([1, 0, 0]),
-    s.Matrix([1, 0, 1]),
-    s.Matrix([1, 1, 0]),
-    s.Matrix([1, 1, 1])
+    s.Matrix([[-1, -1, -1, 1]]).T,
+    s.Matrix([[-1, -1, 1, 1]]).T,
+    s.Matrix([[-1, 1, -1, 1]]).T,
+    s.Matrix([[-1, 1, 1, 1]]).T,
+    s.Matrix([[1, -1, -1, 1]]).T,
+    s.Matrix([[1, -1, 1, 1]]).T,
+    s.Matrix([[1, 1, -1, 1]]).T,
+    s.Matrix([[1, 1, 1, 1]]).T
 ]
 
 
@@ -51,14 +51,35 @@ class Mesh:
         self.faces = faces
         self.plotName = plotName
 
+    def generatePolygons(self) -> [[]]:
+        faces = []
+        for face in self.faces:
+            plane = []
+            for pointIndex in face:
+                v = self.points[pointIndex]
+                plane.append([v[0, 0], v[1, 0], v[2, 0]])
+            faces.append(plane)
+        return faces
+
+    def getNewPoints(self, m: s.Matrix):
+        points = []
+
+        for i in range(len(self.points)):
+            v = m * self.points[i]
+            points.append(v)
+
+        self.points = points
+
 
 def drawPlot(meshes: [Mesh], screenName: str, cameraPosition=None):
     fig = plt.figure(figure=(7, 7))
     ax = plt.axes(projection="3d")
 
+    legend = []
     # Create mesh
     for mesh in meshes:
         drawMesh(ax, mesh)
+        legend.append(mesh.plotName)
 
     ax.set_xlim([-SPACE_LIMIT, SPACE_LIMIT])
     ax.set_ylim([-SPACE_LIMIT, SPACE_LIMIT])
@@ -68,6 +89,7 @@ def drawPlot(meshes: [Mesh], screenName: str, cameraPosition=None):
     ax.set_ylabel('Y')
     ax.set_zlabel('Z')
 
+    plt.legend(labels=legend)
     ax.legend()
     ax.grid()
 
@@ -76,59 +98,63 @@ def drawPlot(meshes: [Mesh], screenName: str, cameraPosition=None):
     plt.show()
 
 
-def getNewPoints(mesh: Mesh, m: s.Matrix) -> Mesh:
-    newMesh: Mesh = mesh
-
-    for i in range(len(newMesh.points)):
-        v = m * newMesh.points[i]
-        newMesh.points[i] = v
-
-    return newMesh
-
-
 def getRotationMatrix(x=None, y=None, z=None) -> s.Matrix:
     rotations: [s.Matrix] = []
 
     if x is not None and x != 0:
         sin = s.sin(x)
         cos = s.cos(x)
-        xR: s.Matrix = s.Matrix([[1, 0, 0],
-                                 [0, cos, -sin],
-                                 [0, sin, cos]])
+        xR: s.Matrix = s.Matrix([[1, 0, 0, 0],
+                                 [0, cos, -sin, 0],
+                                 [0, sin, cos, 0],
+                                 [0, 0, 0, 1]])
         rotations.append(xR)
 
     if y is not None and y != 0:
         sin = s.sin(y)
         cos = s.cos(y)
-        yR: s.Matrix = s.Matrix([[cos, 0, sin],
-                                 [0, 1, 0],
-                                 [-sin, 0, cos]])
+        yR: s.Matrix = s.Matrix([[cos, 0, sin, 0],
+                                 [0, 1, 0, 0],
+                                 [-sin, 0, cos, 0],
+                                 [0, 0, 0, 1]])
         rotations.append(yR)
 
     if z is not None and z != 0:
         sin = s.sin(z)
         cos = s.cos(z)
-        zR: s.Matrix = s.Matrix([[cos, -sin, 0],
-                                 [sin, cos, 0],
-                                 [0, 0, 1]])
+        zR: s.Matrix = s.Matrix([[cos, -sin, 0, 0],
+                                 [sin, cos, 0, 0],
+                                 [0, 0, 1, 0],
+                                 [0, 0, 0, 1]])
         rotations.append(zR)
 
     size = len(rotations)
     if size == 0:
         raise Exception("No angles given")
 
-    m = s.ones(3)
+    m = s.eye(4)
     for v in rotations:
         m = m * v
+
+    return m
+
+
+def generateCube(name: str = "КУБ", color: Color = Color.blue) -> Mesh:
+    mesh = Mesh(VERTICES, color, name, FACES)
+    return mesh
+
+
+def getMovingMatrix(x, y, z) -> s.Matrix:
+    v = [x, y, z]
+
+    m: s.Matrix = s.eye(4)
+    for i in range(3):
+        m[i, 3] = v[i]
+
     return m
 
 
 def drawMesh(ax, mesh: Mesh):
-    faces = []
-    for face in mesh.faces:
-        plane = []
-        for pointIndex in face:
-            plane.append(list(mesh.points[pointIndex]))
-        faces.append(plane)
+    faces = mesh.generatePolygons()
     ax.add_collection3d(Poly3DCollection(faces, alpha=0.9, linewidths=1, edgecolors=Color.black.value,
                                          facecolors=mesh.color.value))
